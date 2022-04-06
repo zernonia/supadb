4cfc005c9a818a4b00e7afb5492bad165d401a15
zernonia <zernonia@gmail.com>
2022-04-06 14:05:59 +0800

    scrape imdb info

---
+++ api/imdb.ts
@@ -0,0 +1,44 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node"
+import { initPage } from "./_lib/chromium"
+
+var hrstart = process.hrtime()
+const s = performance.now()
+export default async (req: VercelRequest, res: VercelResponse) => {
+  const page = await initPage()
+  await page.goto(`https://www.imdb.com/search/title/?genres=comedy&start=1`)
+  await page.waitForSelector(".lister-list")
+
+  await page.waitForSelector(".loadlate")
+  let urls = await page.$$eval(".lister-item", (item) => {
+    // Extract the item from the data
+    return item.map((el) => {
+      let link = "https://www.imdb.com" + el.querySelector(".lister-item-image a")?.getAttribute("href")
+      let id = link.substring(link.indexOf("title/") + 6, link.lastIndexOf("/"))
+      let image =
+        el.querySelector(".lister-item-image img")?.getAttribute("loadlate") ??
+        el.querySelector(".lister-item-image img")?.getAttribute("src")
+      let title = (el.querySelector(".lister-item-header a") as HTMLElement)?.innerText
+      let year = (el.querySelector(".lister-item-header .lister-item-year") as HTMLElement)?.innerText.replace(
+        /[()\ \s-]+/g,
+        ""
+      )
+      let certificate = (el.querySelector(".certificate") as HTMLElement)?.innerText
+      let duration = el.querySelector(".runtime")
+        ? +(el.querySelector(".runtime") as HTMLElement)?.innerText.replace(" min", "")
+        : undefined
+      let genre = (el.querySelector(".genre") as HTMLElement)?.innerText.split(", ")
+      let rating = +(el.querySelector(".ratings-imdb-rating") as HTMLElement)?.innerText
+      let metascore = +(el.querySelector(".metascore") as HTMLElement)?.innerText
+      let description = (el.querySelector(".lister-item-content p:nth-child(4)") as HTMLElement)?.innerText
+      let vote = el.querySelector(".sort-num_votes-visible span:nth-child(2)")
+        ? +(el.querySelector(".sort-num_votes-visible span:nth-child(2)") as HTMLElement)?.innerText.replace(/,/g, "")
+        : undefined
+
+      return { id, image, link, title, year, certificate, duration, genre, rating, metascore, description, vote }
+    })
+  })
+
+  var hrend = process.hrtime(hrstart)
+  console.info("Execution time (hr): %ds %dms", hrend[0], hrend[1] / 1000000)
+  res.json(urls)
+}
