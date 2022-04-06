f8f2bcfb824ac3d3ae1290d3ce734c6c62cfb45d
zernonia <zernonia@gmail.com>
2022-04-07 01:17:22 +0800

    fix var not pass to eval

--- api/steam.ts
+++ api/steam.ts
@@ -1,9 +1,8 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node"
 import { initPage } from "./_lib/chromium"
 
-var hrstart = process.hrtime()
-
 export default async (req: VercelRequest, res: VercelResponse) => {
+  var hrstart = process.hrtime()
   const { query } = req
 
   if (!query.genre || !query.page) {
@@ -18,30 +17,35 @@ export default async (req: VercelRequest, res: VercelResponse) => {
     await page.waitForSelector("#NewReleasesTable")
     await page.waitForNetworkIdle()
 
-    let data = await page.$$eval("#NewReleasesRows > a", (item) => {
-      // Extract the item from the data
-      return item.map((el) => {
-        let link = el.getAttribute("href")
-        let id = +link.match(/app\/(.*?)\//i)[1]
-        let image = el.querySelector(".tab_item_cap_img")?.getAttribute("src")
-        let name = (el.querySelector(".tab_item_name") as HTMLElement)?.innerText
-        let price = (el.querySelector(".discount_final_price") as HTMLElement)?.innerText
-        let tags = (el.querySelector(".tab_item_top_tags") as HTMLElement)?.innerText.split(", ")
-        let platform = [
-          el.querySelector(".platform_img.win") ? "Windows" : null,
-          el.querySelector(".platform_img.mac") ? "Mac" : null,
-          el.querySelector(".platform_img.linux") ? "Linux" : null,
-        ].filter((i) => i)
-        let genre = query.genre
+    let data = await page.$$eval(
+      "#NewReleasesRows > a",
+      (item, query: any) => {
+        // Extract the item from the data
+        return item.map((el) => {
+          let link = el.getAttribute("href")
+          let id = +link.match(/app\/(.*?)\//i)[1]
+          let image = el.querySelector(".tab_item_cap_img")?.getAttribute("src")
+          let name = (el.querySelector(".tab_item_name") as HTMLElement)?.innerText
+          let price = (el.querySelector(".discount_final_price") as HTMLElement)?.innerText
+          let tags = (el.querySelector(".tab_item_top_tags") as HTMLElement)?.innerText.split(", ")
+          let platform = [
+            el.querySelector(".platform_img.win") ? "Windows" : null,
+            el.querySelector(".platform_img.mac") ? "Mac" : null,
+            el.querySelector(".platform_img.linux") ? "Linux" : null,
+          ].filter((i) => i)
 
-        return { id, link, name, image, price, tags, platform, genre }
-      })
-    })
+          let genre = query.genre as string
+          return { id, link, image, name, price, tags, platform, genre }
+        })
+      },
+      query
+    )
 
     var hrend = process.hrtime(hrstart)
     console.info("Execution time (hr): %ds %dms", hrend[0], hrend[1] / 1000000)
     res.json(data)
   } catch (err) {
+    console.log({ err })
     res.statusCode = 500
     res.end(err)
   }
