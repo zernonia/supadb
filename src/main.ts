8cbbf0f15b555885bf3309b2e2e0058ddd50e5c5
zernonia <zernonia@gmail.com>
2022-04-07 17:00:17 +0800

    add css

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from "vue"
 import App from "./App.vue"
 import "virtual:windi.css"
+import "./assets/main.css"
 import { router } from "./plugins/router"
 
 createApp(App).use(router).mount("#app")
