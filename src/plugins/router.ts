6da144c680664753108f318fa087f2060b544a7a
zernonia <zernonia@gmail.com>
2022-04-08 12:45:26 +0800

    add router guard

--- src/plugins/router.ts
+++ src/plugins/router.ts
@@ -1,7 +1,15 @@
+import { store } from "@/store"
 import { createRouter, createWebHistory } from "vue-router"
 import routes from "~pages"
 
 export const router = createRouter({
   history: createWebHistory(),
   routes,
+})
+
+router.beforeEach((to, from) => {
+  const toName = to.name?.toString() ?? ""
+  if (!store.user && toName.includes("settings")) {
+    return { name: "login" }
+  }
 })
