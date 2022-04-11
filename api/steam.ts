9225485e135dc495bb62bceb2c160b05d5350fff
zernonia <zernonia@gmail.com>
2022-04-11 13:00:53 +0800

    update steam to scrap more page at once

--- api/steam.ts
+++ api/steam.ts
@@ -2,6 +2,35 @@ import { supabase } from "./_lib/supabase"
 import { initPage } from "./_lib/chromium"
 import type { VercelRequest, VercelResponse } from "@vercel/node"
 import verify from "./_lib/verify"
+import { Page } from "puppeteer-core"
+
+const scrapData = async (page: Page, query: any) => {
+  return await page.$$eval(
+    "#NewReleasesRows > a",
+    (item, query: any) => {
+      // Extract the item from the data
+      return item.map((el) => {
+        let link = el.getAttribute("href")
+        let id = Number(link?.match(/app\/(.*?)\//i)?.[1])
+        let image =
+          el.querySelector(".tab_item_cap_img")?.getAttribute("src")?.replace("capsule_184x69", "header") ?? null
+        let title = (el.querySelector(".tab_item_name") as HTMLElement)?.innerText ?? null
+        let price =
+          +(el.querySelector(".discount_final_price") as HTMLElement)?.innerText?.replace(/[^\d.-]/g, "") ?? null
+        let tags = (el.querySelector(".tab_item_top_tags") as HTMLElement)?.innerText.split(", ") ?? null
+        let platforms = [
+          el.querySelector(".platform_img.win") ? "Windows" : null,
+          el.querySelector(".platform_img.mac") ? "Mac" : null,
+          el.querySelector(".platform_img.linux") ? "Linux" : null,
+        ].filter((i) => i)
+
+        let genre = query.genre as string
+        return { id, link, image, title, price, tags, platforms, genre }
+      })
+    },
+    query
+  )
+}
 
 export default async (req: VercelRequest, res: VercelResponse) => {
   verify(req, res)
@@ -15,39 +44,21 @@ export default async (req: VercelRequest, res: VercelResponse) => {
 
   try {
     const page = await initPage()
-    await page.goto(`https://store.steampowered.com/tags/en/${query.genre}/#p=${query.page}&tab=NewReleases`)
+    await page.goto(`https://store.steampowered.com/tags/en/${query.genre}/#p=${+query.page * 3}&tab=NewReleases`)
 
     await page.waitForSelector("#NewReleasesTable")
     await page.waitForNetworkIdle()
     await page.waitForTimeout(500)
 
-    let data = await page.$$eval(
-      "#NewReleasesRows > a",
-      (item, query: any) => {
-        // Extract the item from the data
-        return item.map((el) => {
-          let link = el.getAttribute("href")
-          let id = Number(link?.match(/app\/(.*?)\//i)?.[1])
-          let image =
-            el.querySelector(".tab_item_cap_img")?.getAttribute("src")?.replace("capsule_184x69", "header") ?? null
-          let title = (el.querySelector(".tab_item_name") as HTMLElement)?.innerText ?? null
-          let price =
-            +(el.querySelector(".discount_final_price") as HTMLElement)?.innerText?.replace(/[^\d.-]/g, "") ?? null
-          let tags = (el.querySelector(".tab_item_top_tags") as HTMLElement)?.innerText.split(", ") ?? null
-          let platforms = [
-            el.querySelector(".platform_img.win") ? "Windows" : null,
-            el.querySelector(".platform_img.mac") ? "Mac" : null,
-            el.querySelector(".platform_img.linux") ? "Linux" : null,
-          ].filter((i) => i)
+    let data = await scrapData(page, query)
 
-          let genre = query.genre as string
-          return { id, link, image, title, price, tags, platforms, genre }
-        })
-      },
-      query
-    )
+    await Promise.all([page.click("#NewReleases_btn_next"), page.waitForNavigation()])
+    let data2 = await scrapData(page, query)
 
-    const result = await supabase.from("steam").upsert(data)
+    await Promise.all([page.click("#NewReleases_btn_next"), page.waitForNavigation()])
+    let data3 = await scrapData(page, query)
+
+    const result = await supabase.from("steam").upsert([...data, ...data2, ...data3])
     if (result.error) throw new Error(result.error.message)
 
     var hrend = process.hrtime(hrstart)
